b698b3e92c7ebd723bfea35c78470da0913cd48e
Furkan <58880808+furkanabanoz@users.noreply.github.com>
2022-03-08 00:35:33 +0300

    Create math_date.cs

---
+++ math_date.cs
@@ -0,0 +1,44 @@
+using System;
+
+namespace math_date
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          //DateTimeMethod
+          Console.WriteLine(DateTime.Now);
+          Console.WriteLine(DateTime.Now.Date);
+          Console.WriteLine(DateTime.Now.Day);
+          Console.WriteLine(DateTime.Now.Month);
+          Console.WriteLine(DateTime.Now.Year);
+          Console.WriteLine(DateTime.Now.Hour);
+          Console.WriteLine(DateTime.Now.Minute);
+          Console.WriteLine(DateTime.Now.Second);
+          Console.WriteLine(DateTime.Now.DayOfWeek);
+          Console.WriteLine(DateTime.Now.DayOfYear);
+          Console.WriteLine(DateTime.Now.ToLongDateString);
+          Console.WriteLine(DateTime.Now.ToShortDateString);
+          
+          //MathLibary
+          
+          Console.WriteLine(Math.Abs(-25));
+          Console.WriteLine(Math.Sin(10));
+          Console.WriteLine(Math.Cos(10));
+          Console.WriteLine(Math.Tan(10));
+          Console.WriteLine(Math.Ceiling(10.3));
+          Console.WriteLine(Math.Round(10.3));
+          Console.WriteLine(Math.Floor(10.3));
+          Console.WriteLine(Math.Max(10,3));
+          Console.WriteLine(Math.Min(10,3));
+          Console.WriteLine(Math.Pow(2,3));
+          Console.WriteLine(Math.Sqrt(9));
+          Console.WriteLine(Math.Log(3));
+          Console.WriteLine(Math.Log10(10));
+          
+          
+        
+        
+        }
+    }
+}
